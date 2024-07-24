063647de409895b219f7fd304c4d8c89b11eb9fc
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-07-24 20:56:13 -0300

    :sparkles: [FEAT] MERN Login and Register Examples.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,30 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
+import { CandidateContextProvider } from '../context/candidateContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Register from './pages/RegisterCandidate';
+import Login from './pages/Login';
+import Candidate from './pages/Candidate';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials = true;
 
 function App() {
-
-  return (
-    <>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/login' element={<Login/>} />
-      </Routes>
-    </>
-  )
+    return (
+        <CandidateContextProvider>
+            <Navbar />
+            <Toaster position='top-center' toastOptions={{ duration: 2000 }} />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/candidate' element={<Candidate />} />
+            </Routes>
+        </CandidateContextProvider>
+    );
 }
 
-export default App
+export default App;
