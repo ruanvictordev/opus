d904fd834c5c82fff6f673e1684f3b226a7bccac
brunolimapinheiro <brunol21082001@gmail.com>
2024-07-30 14:22:53 -0300

    fiz apenas pequenas alterações

--- client/src/components/Header/Navbar.tsx
+++ client/src/components/Header/Navbar.tsx
@@ -1,10 +1,11 @@
 import { Nav, P, Div, StyledLink, Button } from "./styles";
+import img from "../../assets/img/opus_logo.jpeg";
 
 export default function Navbar() {
   return (
     <>
       <Nav>
-        <P>oi</P>
+        <img src={img} height={50} width={50} alt="" />
         <Div>
           <StyledLink to="">Destaques</StyledLink>
           <StyledLink to="">Vagas</StyledLink>
--- client/src/components/Main/styled.ts
+++ client/src/components/Main/styled.ts
@@ -1,23 +1,24 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
- 
-    
-    height: 100vh;
-    border: 2px black solid;
-`
+  height: 100vh;
+  border: 1px black solid;
+  padding: 10px;
+`;
 export const Div = styled.div`
-    display: flex;
-   
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
 export const Text = styled.h3`
-    color: #ffffff;
-    border: 2px black solid;
-
-`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 28px;
+  margin: 0px 30px 70px 60px;
+`;
 export const Img = styled.img`
-    width: 39%;
-    margin: 50px 95px;
-    float: right;
-
-`
+  width: 39%;
+  margin: 50px 95px;
+  float: right;
+`;
