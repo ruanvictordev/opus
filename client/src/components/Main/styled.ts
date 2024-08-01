24ecb3998fe6726df9141bf958667731bad4e792
kelySoare5 <kelysoares688@gmail.com>
2024-07-31 19:40:35 -0500

    Ajuste na centralização do texto

--- client/src/components/Main/styled.ts
+++ client/src/components/Main/styled.ts
@@ -1,66 +1,51 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-/* <<<<<<< HEAD */
+  
   height: 100vh;
-  border: 1px black solid;
-  padding: 10px;
-`;
+  
+`
 export const Div = styled.div`
+  /* margin: auto;
+  border: 1px solid black;
   display: flex;
   justify-content: space-around;
+  align-items: center; */
+  display: flex;
+  margin: 50px auto;
   align-items: center;
-`;
+  justify-content: center;
+  flex-wrap: wrap;
 
-export const Text = styled.h3`
+  
+  @media (max-width: 768px) {
+  flex-direction: column;
+  align-items: flex-start;
+}
+    
+`
+export const Text = styled.h1`
+  /* border: 1px solid black; */
   color: #ffffff;
-  font-weight: bold;
-  font-size: 28px;
-  margin: 0px 30px 70px 60px;
-`;
+  flex: 1;
+  margin: 0px 50px 70px 190px;
+  text-align: center;
+  //margin-right: 20px;
+
+  @media (max-width: 768px) {
+  margin-right: 0;
+  margin-bottom: 10px;
+}
+
+`
 export const Img = styled.img`
   width: 39%;
   margin: 50px 95px;
   float: right;
-`;
-// =======
-//     height: 100vh;
-    
-// `
-// export const Div = styled.div`
-//     display: flex;
-//     margin: auto;
-//     align-items: center;
-//     justify-content: center;
-//     flex-wrap: wrap;
-
-    
-//     @media (max-width: 768px) {
-//     flex-direction: column;
-//     align-items: flex-start;
-//   }
-   
-// `
-// export const Text = styled.h1`
-//     color: #ffffff;
-//     flex: 1;
-//     margin-right: 20px;
-
-//     @media (max-width: 768px) {
-//     margin-right: 0;
-//     margin-bottom: 10px;
-//   }
-
-// `
-// export const Img = styled.img`
-//     width: 39%;
-//     margin: 50px 95px;
-//     float: right;
-//     /* border: 2px black solid; */
+  /* border: 2px black solid; */
 
 
-//     @media (max-width: 768px) {
-//     width: 100%;
-//   }
-// `
-// >>>>>>> e5f20c3 (Ajuste no styled texto)
+  @media (max-width: 768px) {
+  width: 100%;
+}
+`
