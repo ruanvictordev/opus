562362627739363bf9f0e034a3e43ca1c3ac3ed6
kelySoare5 <kelysoares688@gmail.com>
2024-08-14 18:38:57 -0500

    ajuste no tipo de candidato

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -3,6 +3,17 @@ import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import { FiX } from "react-icons/fi";
 
+
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+
+
+
 interface ModalProps {
   handleOpen: () => void;
   open: boolean;
@@ -46,11 +57,40 @@ export default function ModalLogin({
                 <S.btnEntrar onClick={handModalForm}>Entrar</S.btnEntrar>
               </S.cx>
 
+<<<<<<< HEAD
               <S.divCadastros>
                 <S.esquecerSenha>Cadastro Candidato</S.esquecerSenha>
 
                 <S.esquecerSenha>Cadastro Empresa</S.esquecerSenha>
               </S.divCadastros>
+=======
+              {/* <S.divCadastros>
+              <S.esquecerSenha>
+                  Cadastro Candidato
+                </S.esquecerSenha> 
+                
+                <S.esquecerSenha>
+                  Cadastro Empresa
+                </S.esquecerSenha>
+              </S.divCadastros> */}
+              <FormControl>
+                <FormLabel id="demo-row-radio-buttons-group-label">Tipo de Candidato</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group">
+                  <FormControlLabel value="Empregado" control={<Radio />} label="Empregado" />
+                  <FormControlLabel value="Empresa" control={<Radio />} label="Empresa" />
+                  {/* <FormControlLabel value="other" control={<Radio />} label="Other" /> */}
+                  {/* <FormControlLabel
+                    value="disabled"
+                    disabled
+                    control={<Radio />}
+                    label="other"
+                  /> */}
+                </RadioGroup>
+              </FormControl>
+>>>>>>> 343cad6 (Add input Radio)
             </S.conteiner>
           </S.Modal>
         </Box>
